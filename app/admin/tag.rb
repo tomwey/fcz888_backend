7cd70a8c3601f0ee05b44867ac96ccc3c5e30c77
tomwey <tomwey@163.com>
2019-03-30 07:01:40 +0800

    ccc

---
+++ app/admin/tag.rb
@@ -0,0 +1,24 @@
+ActiveAdmin.register Tag do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :name, :memo
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+form do |f|
+  f.semantic_errors
+  f.inputs do
+    f.input :name, placeholder: '例如：高额度'
+    f.input :memo, placeholder: '选填'
+  end
+  actions
+end
+
+end
