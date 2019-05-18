f39f2eb23309f3ec6e888ac31b5d1c587e52825c
tomwey <tomwey@163.com>
2019-05-18 10:29:00 +0800

    ccc

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -20,6 +20,7 @@ index do
   selectable_column
   column('#',:id)
   column :uid, sortable: false
+  column :mobile, sortable: false
   column :name, sortable: false
   column :idcard, sortable: false
   column '注册渠道' do |o|
@@ -31,7 +32,39 @@ index do
   end
   # column :private_token, sortable: false
   column :verified, sortable: false
-  actions
+  defaults: false do |o|
+    # item '查看'
+    if o.verified
+      item "禁用", block_admin_user_path(o), method: :put, data: { confirm: '你确定吗？' }, class: 'danger'
+    else
+      item "启用", unblock_admin_user_path(o), method: :put, data: { confirm: '你确定吗？' }
+    end
+    item "删除", admin_user_path(o), method: :delete, data: { confirm: '你确定吗？' }
+  end
+end
+
+# 禁用账户
+batch_action :block do |ids|
+  batch_action_collection.find(ids).each do |o|
+    o.block!
+  end
+  redirect_to collection_path, alert: "已禁用"
+end
+member_action :block, method: :put do
+  resource.block!
+  redirect_to collection_path, notice: '禁用成功'
+end
+
+# 启用账户
+batch_action :unblock do |ids|
+  batch_action_collection.find(ids).each do |o|
+    o.unblock!
+  end
+  redirect_to collection_path, alert: "已启用"
+end
+member_action :unblock, method: :put do
+  resource.unblock!
+  redirect_to collection_path, notice: '启用成功'
 end
 
 
