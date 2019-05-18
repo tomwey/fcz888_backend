acc987eb14b01c1158445df3c5a2ccb6bcb6a662
tomwey <tomwey@163.com>
2019-05-18 10:04:44 +0800

    ccc

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -12,4 +12,23 @@ ActiveAdmin.register User do
 #   permitted
 # end
 
+index do
+  selectable_column
+  column('#',:id)
+  column :uid, sortable: false
+  column :name, sortable: false
+  column :idcard, sortable: false
+  column '注册渠道' do |o|
+    o.channel.try(:name)
+  end
+  column :created_at
+  column '银行信息' do |o|
+    raw("银行卡号: #{o.bank_no}<br>银行预留手机: #{o.bank_mobile}<br>开卡信息: #{o.bank_info}")
+  end
+  column :private_token, sortable: false
+  column :verified, sortable: false
+  actions
+end
+
+
 end
